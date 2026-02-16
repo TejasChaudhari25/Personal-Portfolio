39589be4550dc95ca6d1febd03b3bb29b62bf77c
Tejas Chaudhari <138961185+TejasChaudhari25@users.noreply.github.com>
2026-02-16 12:47:35 +0530

    Add dark mode toggle functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById("darkToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if(document.body.classList.contains("dark")){
+    toggle.innerText = "☀️";
+  } else {
+    toggle.innerText = "🌙";
+  }
+});
